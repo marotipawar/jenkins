1ed180c69f98b2e0fdd9d79d081d0cd837006eab
marotipawar <pawarmw@gmail.com>
2023-09-19 08:53:30 +0530

    jenkin test

---
+++ src/main/java/com/maroti/App.java
@@ -0,0 +1,13 @@
+package com.maroti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/maroti/AppTest.java
@@ -0,0 +1,12 @@
+package com.maroti;
+
+import org.testng.annotations.Test;
+
+@Test
+public class AppTest {
+
+    @Test
+    public void m1() {
+        System.out.println("Test completed successfully............By Maroti Pawar");
+    }
+}
